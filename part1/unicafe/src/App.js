7b289bfe3e59fda663367a4a34e139ab4b43aa38
Joey <erjo.guce214@gmail.com>
2022-09-03 23:23:11 -0400

    separated event handlers into functions

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,12 +4,17 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [netural, setNeutal] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutal(netural + 1)
+  const increaseBad = () => setBad(bad + 1)
+
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutal(netural + 1)}>netural</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={increaseGood}>good</button>
+      <button onClick={increaseNeutral}>netural</button>
+      <button onClick={increaseBad}>bad</button>
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {netural}</p>
