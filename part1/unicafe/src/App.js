c45874908aa400e3dfb251c0b6a1757d038f92f3
Joey <erjo.guce214@gmail.com>
2022-09-04 13:21:01 -0400

    Added all state

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,30 +1,45 @@
 import { useState } from 'react'
 
-const Button = (props) => {
+const Button = ({ onClick, text }) => {
   return (
-    <button onClick={props.onClick}>{props.text}</button>
+    <button onClick={onClick}>{text}</button>
   )
 }
 
 const App = () => {
   const [good, setGood] = useState(0)
-  const [netural, setNeutal] = useState(0)
+  const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const increaseGood = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutal(netural + 1)
-  const increaseBad = () => setBad(bad + 1)
+  const increaseGood = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+
+  const increaseBad = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
       <Button onClick={increaseGood} text='good' />
-      <Button onClick={increaseNeutral}text='neutral' />
+      <Button onClick={increaseNeutral} text='neutral' />
       <Button onClick={increaseBad} text='bad' />
       <h2>statistics</h2>
       <p>good {good}</p>
-      <p>neutral {netural}</p>
+      <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good - bad) / all}</p>
+      <p>positive {(good / all) * 100}%</p>
     </div>
   );
 }
