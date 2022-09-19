e7538e83b40133fc35d726a7f2aa6f2c6c169571
Joey <erjo.guce214@gmail.com>
2022-09-19 13:27:35 -0400

    Added to courses object

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,9 +1,9 @@
 const Total = ({parts}) => {
+  
   let exercisesArr = parts.map(part => part.exercises)
   let totalExercises = exercisesArr.reduce(
     (prev, curr) => prev + curr, 0);
 
-  console.log(totalExercises)
   return (
     <p>
       Number of exercises {totalExercises}
