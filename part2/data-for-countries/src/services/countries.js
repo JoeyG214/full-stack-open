468b213ce3edca2f225e18c2c7533343358b9ce2
Joey <erjo.guce214@gmail.com>
2022-09-23 21:54:42 -0400

    Setup communication with backend

---
+++ part2/data-for-countries/src/services/countries.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const baseURL = 'http://localhost:3001/countries'
+
+const getAll = () => {
+    const request = axios.get(baseURL)
+    return request.then(response => response.data)
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getAll }
