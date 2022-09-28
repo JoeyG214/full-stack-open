6fe56c810347b0eff6c2628f90034b8c2cce51d5
Joey <erjo.guce214@gmail.com>
2022-09-28 14:59:24 -0400

    Added basic functionality

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios';
+import CountryInfo from './components/CountryInfo';
 
-function App() {
+const App = () => {
+  
+  const [countries, setCountries] = useState([])
+  const [country, setCountry] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleCountryChange = (event) => {
+    setCountry(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search Countries</h1>
+      <div className='search-countries'>
+        Country Name:
+        <input value={country} onChange={handleCountryChange} />
+      </div>
+      <h2>Countries</h2>
+      <CountryInfo countries={countries} />
     </div>
   );
 }
