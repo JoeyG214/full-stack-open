2367f0651936c928532ed1cd495ad93d47985c1f
Joey <erjo.guce214@gmail.com>
2022-09-30 12:00:44 -0400

    Add feature to delete note

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -55,10 +55,14 @@ app.get('/info', (request, response) => {
   )
 })
 
-// app.delete('/api/persons/:id', (request, response) => {
-//   const id = Number(request.params.id)
-//   persons = persons.filter(person => person.id !== id)
-//   response.status(204).end()
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+// app.post('/api/persons', (request, response) => {
+
 // })
 
 app.listen(PORT, () => {
