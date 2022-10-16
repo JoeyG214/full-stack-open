0e7966af57cd92347af8a2f5af7c3ac245423242
JoeyG <erjo.guce214@gmail.com>
2022-10-15 23:48:04 -0400

    Added feature to add a person

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
+app.use(express.json())
+
 let persons = [
   { 
     "id": 1,
@@ -61,9 +63,30 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-// app.post('/api/persons', (request, response) => {
+const generateID = () => {
+  const maxID = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxID + 1
+}
 
-// })
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const person = {
+    id: generateID(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
