191ba36e59003d3fce637e20087d57bec6f02e6e
JoeyG <erjo.guce214@gmail.com>
2022-10-24 02:36:09 -0400

    Added database validation

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -65,21 +65,13 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response, next) => {
-  Person
-    .findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!(body.name && body.number)) {
-    return response.status(400).json({ error: 'content missing' })
-  }
+  // Validation now done via MongoDB
+  // if (body.name === undefined || body.number === undefined) {
+  //   return response.status(400).json({ error: 'content missing' })
+  // }
   // else if (persons.some(person => person.name === body.name )) {
   //   return response.status(400).json({ error: 'name must be unique' })
   // }
@@ -98,16 +90,21 @@ app.post('/api/persons', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
-  const person = {
-    name: body.name,
-    number: body.number
-  }
+  const { name, number } = request.body
 
   Person
-    .findByIdAndUpdate(request.params.id, person, { new: true })
+    .findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
@@ -123,6 +120,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
 
 const url = process.env.MONGODB_URI 
 
@@ -12,8 +16,17 @@ mongoose
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true
+  }
 })
 
 personSchema
@@ -24,5 +37,6 @@ personSchema
       delete returnedObject.__v
     }
   })
-  
+personSchema.plugin(uniqueValidator);
+
 module.exports = mongoose.model('Person', personSchema)
