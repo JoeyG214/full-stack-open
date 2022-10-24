f6baee9bc41191bd2470942be42b569f35c39016
JoeyG <erjo.guce214@gmail.com>
2022-10-23 20:29:44 -0400

    Make persons delete from database

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -60,10 +60,12 @@ app.get('/info', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
-  //Add catch
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
