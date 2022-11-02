8b35889e3b96f66d6bfcb0c03150bb9d6081cc6b
JoeyG <erjo.guce214@gmail.com>
2022-11-02 16:33:05 -0400

    Fixed issues

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: String,
   name: String,
   passwordHash: String,
-  notes: [
+  blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Blog'
@@ -12,15 +12,16 @@ const userSchema = new mongoose.Schema({
   ],
 })
 
-userSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-    // the passwordHash should not be revealed
-    delete returnedObject.passwordHash
-  }
-})
+userSchema
+  .set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+      // the passwordHash should not be revealed
+      delete returnedObject.passwordHash
+    }
+  })
 
 const User = mongoose.model('User', userSchema)
 
