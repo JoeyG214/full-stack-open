d34680e25c39d689b74cb576c2568de0cba267c4
JoeyG <erjo.guce214@gmail.com>
2022-11-19 18:48:09 -0500

    Added request logging middleware

--- part3/phonebook/app.js
+++ part3/phonebook/app.js
@@ -22,6 +22,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+//app.use(middleware.requestLogger)
 
 morgan.token('data', (request) => {
   const { body } = request
--- part3/phonebook/utils/middleware.js
+++ part3/phonebook/utils/middleware.js
@@ -1,5 +1,13 @@
 const logger = require('./logger')
 
+// const requestLogger = (request, response, next) => {
+//   logger.info('Method:', request.method)
+//   logger.info('Path:  ', request.path)
+//   logger.info('Body:  ', request.body)
+//   logger.info('---')
+//   next()
+// }
+
 // handler of requests with unknown endpoint
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -19,6 +27,7 @@ const errorHandler = (error, request, response, next) => {
 }
 
 module.exports = {
+  //requestLogger,
   unknownEndpoint,
   errorHandler
 }
