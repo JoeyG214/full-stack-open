47f9e42e5023489489573123b62e5244cb6b5767
JoeyG <erjo.guce214@gmail.com>
2022-12-17 02:29:36 -0500

    Add mostBlogs, mostLikes functions for testing

--- part4/bloglist/utils/for_testing.js
+++ part4/bloglist/utils/for_testing.js
@@ -1,3 +1,16 @@
+const initialBlogs = [
+  {
+    content: 'HTML is easy',
+    date: new Date(),
+    important: false
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    date: new Date(),
+    important: true
+  }
+]
+
 const totalLikes = (blogs) => {
   let likes = 0
   for (let blog of blogs) {
@@ -16,22 +29,46 @@ const favoriteBlogs = (blogs) => {
   return favoriteBlog
 }
 
-const nonExistingId = () => {
-  return 1
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  else {
+    let authorCounts = blogs.reduce((authorCount, blog) => {
+      authorCount[blog.author] = (authorCount[blog.author] || 0) + 1
+      return authorCount
+    }, {})
+    let maxCount = Math.max(...Object.values(authorCounts))
+    let mostFrequent = Object.keys(authorCounts).filter(author => authorCounts[author] === maxCount)
+    return {
+      author: mostFrequent[0],
+      blogs: maxCount
+    }
+  }
 }
 
-const notesInDB = () => {
-  return 1
-}
-
-const usersInDB = () => {
-  return 1
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  else {
+    let likesCounts = blogs.reduce((likesCount, blog) => {
+      likesCount[blog.author] = (likesCount[blog.author] || 0) + blog.likes
+      return likesCount
+    }, {})
+    let maxCount = Math.max(...Object.values(likesCounts))
+    let mostLiked = Object.keys(likesCounts).filter(author => likesCounts[author] === maxCount)
+    return {
+      author: mostLiked[0],
+      likes: maxCount
+    }
+  }
 }
 
 module.exports = {
+  initialBlogs,
   totalLikes,
   favoriteBlogs,
-  nonExistingId,
-  notesInDB,
-  usersInDB
+  mostLikes,
+  mostBlogs
 }
